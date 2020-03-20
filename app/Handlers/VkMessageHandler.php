d5d19347161b79d55a07dc9aa5d1dd7fa53a3450
NDS <dddddd1997@gmail.com>
2020-03-20 05:07:44 +0300

    filter trigger words for buttons 2

--- app/Handlers/VkMessageHandler.php
+++ app/Handlers/VkMessageHandler.php
@@ -127,7 +127,7 @@ class VkMessageHandler
     private function generateKeyboardJson($newStateId)
     {
         $possibleStates = $this->generateTriggerWordsForState($newStateId)->filter(function ($value, $key) {
-            return $value->id > 8;
+            return $value->id > 7;
         });
         $buttons = [];
         foreach ($possibleStates as $currState) {
